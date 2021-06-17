d951a704f947ffaa0af3ad9b8ea6c21ca9b051c9
juliojimenez98 <juliojimenez1998@gmail.com>
2021-06-17 12:32:05 -0400

    layout login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
+import { FormLoginComponent } from './auth/components/form-login/form-login.component';
+import { FormRegisterComponent } from './auth/components/form-register/form-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    FormLoginComponent,
+    FormRegisterComponent
   ],
   imports: [
     BrowserModule,
